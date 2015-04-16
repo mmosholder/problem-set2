7f40a7b81a98af004b46a0d7f582bf5946876a37
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-04-16 09:26:58 -0600

    initial problem work

---
+++ main.js
@@ -0,0 +1,22 @@
+var firstReverse = function(str) {
+	return str.split('').reverse().join('');
+};
+
+var swapCase = function(str) {
+	var strArr = str.split('');
+	var newArr = [];
+	for(var i = 0; i < strArr.length; i++) {
+		if(strArr[i].toUpperCase() === strArr[i]) {
+			newArr.push(strArr[i].toLowerCase());
+		}
+		else {
+			newArr.push(strArr[i].toUpperCase());
+		}
+	}
+
+	return newArr.join('');
+};
+
+var letterCount = function(str) {
+
+};
